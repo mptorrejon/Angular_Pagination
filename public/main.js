14d29928d7dc0be5aecbdb6b6f6d369808a0d552
mptorrejon <mptorrejon@gmail.com>
2016-02-02 21:45:11 -0500

    added comments

--- public/main.js
+++ public/main.js
@@ -1,16 +1,16 @@
 var App = angular.module("app", []);
 App.controller('appCtrl', function($scope, $http){
-    $scope.pageNumber = 1;
+    $scope.pageNumber = 1; //default pagen number
     $scope.displayOptions = [
         {option: 10}, 
         {option: 15}, 
         {option:20}, 
         {option:25}
-    ];
-    $scope.display = 10;
-    $scope.data  = "";
-    var myd = ""
-    $scope.init = function(){
+    ]; //data to build total records per page display
+    $scope.display = 10; // default for total records to display
+    $scope.data  = ""; //initialize data holder
+    var myd = "" //initialize tmp data holder
+    $scope.init = function(){ //wraps API get so it can be call when total records per page change 
         $http.get("https://restcountries.eu/rest/v1/all")
         .success(function(mydata){
             myd = mydata;
@@ -21,8 +21,8 @@ App.controller('appCtrl', function($scope, $http){
             console.log(myerr);
         });    
     }
-    $scope.init();
-    $scope.Arrow = function(which){
+    $scope.init(); //tables gets bootstrapped
+    $scope.Arrow = function(which){ //arrow table controllers
         switch(which){
             case 'left':
                 $scope.data = myd.slice( ($scope.pageNumber*$scope.display)-($scope.display*2), $scope.display*($scope.pageNumber-1) );
@@ -34,7 +34,7 @@ App.controller('appCtrl', function($scope, $http){
             break;
         }    
     }
-    $scope.changedValue = function(val){
+    $scope.changedValue = function(val){//dropdown records per page controller
         $scope.display = val.option;
         $scope.init();
     }
