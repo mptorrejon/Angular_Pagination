4baaf1e45edd04337ccdee651c2543c2adb8d473
mptorrejon <mptorrejon@gmail.com>
2016-02-02 21:33:14 -0500

    FIXED# +total records display per page.
    NEED#
    +to implement zero counter to show

--- public/main.js
+++ public/main.js
@@ -1,23 +1,31 @@
 var App = angular.module("app", []);
 App.controller('appCtrl', function($scope, $http){
     $scope.pageNumber = 1;
+    $scope.displayOptions = [
+        {option: 10}, 
+        {option: 15}, 
+        {option:20}, 
+        {option:25}
+    ];
     $scope.display = 10;
     $scope.data  = "";
     var myd = ""
-    $http.get("https://restcountries.eu/rest/v1/all")
+    $scope.init = function(){
+        $http.get("https://restcountries.eu/rest/v1/all")
         .success(function(mydata){
             myd = mydata;
             $scope.data = mydata.slice(0, $scope.display-1);
+            console.log($scope.data);
         })
         .error(function(myerr){
             console.log(myerr);
-        });
+        });    
+    }
+    $scope.init();
     $scope.Arrow = function(which){
-        console.log(myd);
-        
         switch(which){
             case 'left':
-                $scope.data = myd.slice( ($scope.pageNumber*$scope.display)-20, $scope.display*($scope.pageNumber-1) );
+                $scope.data = myd.slice( ($scope.pageNumber*$scope.display)-($scope.display*2), $scope.display*($scope.pageNumber-1) );
                 $scope.pageNumber--;
             break
             case 'right':
@@ -25,11 +33,9 @@ App.controller('appCtrl', function($scope, $http){
                 $scope.pageNumber++;
             break;
         }    
-        
-        
-        console.log($scope.data);
     }
-    $scope.displayMore = function(){
-        console.log("change records per page");
+    $scope.changedValue = function(val){
+        $scope.display = val.option;
+        $scope.init();
     }
 });
